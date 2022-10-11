e2ad7d29ea89691a818a933a1c676b136201278e
unknown <kateplum99@gmail.com>
2022-10-11 18:45:36 +0200

    first commit

--- seeds/index.js
+++ seeds/index.js
@@ -16,6 +16,7 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 
 
+
 const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 300; i++) {
